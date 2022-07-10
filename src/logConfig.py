8a34416e91580f0577f22b6bb0aaf84e9489c91e
Huy Ngo <huybngo@yahoo.com>
2022-07-10 16:05:17 -0700

    Add logging

---
+++ src/logConfig.py
@@ -0,0 +1,26 @@
+from typing import Any
+from pydantic import BaseModel
+
+class LogConfig(BaseModel):
+    """Logging configuration"""
+    logFormat: str = "%(levelprefix)s %(asctime)s | %(message)s"
+    version = 1
+    disable_existing_loggers = False
+    formatters = {
+      "default": {
+          "()": "uvicorn.logging.DefaultFormatter",
+          "fmt": logFormat,
+          "datefmt": "%Y-%m-%d %H:%M:%S",
+      },
+    }
+    handlers = {
+      "default": {
+          "formatter": "default",
+          "class": "logging.StreamHandler",
+          "stream": "ext://sys.stdout",
+      },
+    }
+    loggers: dict[str, dict[str, Any]] = {}
+
+    def addLogger(self, loggerName: str, logLevel: int | str):
+      self.loggers[loggerName] = { "handlers": ["default"], "level": logLevel }
--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException, status
+import logging
+from logging.config import dictConfig
+from logConfig import LogConfig
 import requests
 import entity
 import vinHelpers
 import queries
 
 app = FastAPI()
+
+# Set up logging 
+loggerName, logConfig = __name__, LogConfig()
+logConfig.addLogger(loggerName, "INFO")
+
+dictConfig(logConfig.dict())
+logger = logging.getLogger(loggerName)
+
+# Set up connection to database
 connection = queries.connectToVinDatabase("vinCache.db")
 
 class LookupResponse(BaseModel):
@@ -29,7 +41,10 @@ def __validateVinFormat(vin: str):
 
 @app.on_event("shutdown")
 def shutdown():
-  connection.close()
+  logger.info("Shutting down service.")
+  if connection is not None:
+    logger.info("Closing database connection.")
+    connection.close()
 
 @app.get("/lookup/{vin}", status_code=status.HTTP_200_OK)
 def lookup(vin: str):
@@ -37,6 +52,7 @@ def lookup(vin: str):
   
   cacheVin = queries.getVin(connection, vin)
   if cacheVin is not None:
+    logger.info("Got vin %s from cache.", vin)
     return LookupResponse(**cacheVin.dict(), cachedResult=True)
 
   response = requests.get(f"https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/{vin}?format=json")
@@ -49,6 +65,7 @@ def lookup(vin: str):
                            model=jsonObj["Model"],
                            modelYear=jsonObj["ModelYear"],
                            bodyClass=jsonObj["BodyClass"])
+    logger.info("Inserting vin %s to cache.", vin)
     queries.insertVin(connection, entityVin)
     
     return LookupResponse(**entityVin.dict(), cachedResult=False)
@@ -64,4 +81,5 @@ def remove(vin: str):
     isVinRemoved = queries.removeVin(connection, vin)
     return RemoveResponse(vin=vin, cacheDeleteSuccess=isVinRemoved)
   except Exception as ex:
+    logger.warning(f"Error trying to remove vin {vin}. Error: %s", ex)
     return RemoveResponse(vin=vin, cacheDeleteSuccess=False)
